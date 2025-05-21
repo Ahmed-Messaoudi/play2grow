1376af12b9d8e4a40f71f7934bf004c833f87ab5
Ahmed-Messaoudi <ahmed.massoudi@horizon-tech.tn>
2025-05-21 13:46:53 +0100

    levels models migration

---
+++ packages/backend/prisma/migrations/20250519113906_init_quiz_models/migration.sql
@@ -0,0 +1,38 @@
+-- CreateTable
+CREATE TABLE `Quiz` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `level` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `Quiz_level_key`(`level`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Question` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `quizId` INTEGER NOT NULL,
+    `text` VARCHAR(191) NOT NULL,
+    `type` ENUM('TEXT', 'IMAGE', 'COLOR') NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Option` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `questionId` INTEGER NOT NULL,
+    `text` VARCHAR(191) NULL,
+    `imageUrl` VARCHAR(191) NULL,
+    `isCorrect` BOOLEAN NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Question` ADD CONSTRAINT `Question_quizId_fkey` FOREIGN KEY (`quizId`) REFERENCES `Quiz`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Option` ADD CONSTRAINT `Option_questionId_fkey` FOREIGN KEY (`questionId`) REFERENCES `Question`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
